56376432e5932a0d6f4a9e442972d5f1580895aa
diegocamomila <diego.camomila@gmail.com>
2022-12-01 18:18:58 -0300

    FEAT: Implementará a classe Queue em ting_file_management/file_management.py

--- ting_file_management/file_management.py
+++ ting_file_management/file_management.py
@@ -1,2 +1,15 @@
+import sys
+
+
 def txt_importer(path_file):
     """Aqui irá sua implementação"""
+
+    if ".txt" not in path_file.lower():
+        print("Formato inválido", file=sys.stderr)
+
+    try:
+        with open(path_file) as file_noticias:
+            data = file_noticias.read()
+            return data.split("\n")
+    except FileNotFoundError:
+        print(f"Arquivo {path_file} não encontrado", file=sys.stderr)
