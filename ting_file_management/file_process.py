e02742e330243ab475cbc843cfd6c9c92c5d60df
diegocamomila <diego.camomila@gmail.com>
2022-12-01 20:11:00 -0300

    FEAT: Implementará a classe Queue em ting_file_management/file_process.py

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -1,5 +1,20 @@
+import sys
+from ting_file_management.file_management import txt_importer
+
+
 def process(path_file, instance):
     """Aqui irá sua implementação"""
+    for process in instance._data:
+        if path_file in process["nome_do_arquivo"]:
+            return path_file
+    data = {
+        "nome_do_arquivo": path_file,
+        "qtd_linhas": len(txt_importer(path_file)),
+        "linhas_do_arquivo": txt_importer(path_file),
+    }
+    instance.enqueue(data)
+
+    print(data, file=sys.stdout)
 
 
 def remove(instance):
