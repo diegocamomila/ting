612cb3b2ef9f0843c0a73603604aa3e24c633583
diegocamomila <diego.camomila@gmail.com>
2022-12-02 04:06:18 -0300

    FEAT: Implementará a funçao exists_word em ting_word_searches/word_search.py

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -1,5 +1,21 @@
 def exists_word(word, instance):
     """Aqui irá sua implementação"""
+    list_text = []
+    occurrences = []
+    for index, line in enumerate(instance._data[0]["linhas_do_arquivo"]):
+        if word.lower() in line.lower():
+            occurrences.append({"linha": index + 1})
+    for index, line in enumerate(instance._data[0]["linhas_do_arquivo"]):
+        if word.lower() not in line.lower():
+            return list_text
+        else:
+            data = {
+                "palavra": word,
+                "arquivo": instance._data[0]["nome_do_arquivo"],
+                "ocorrencias": occurrences,
+            }
+            list_text.append(data)
+            return list_text
 
 
 def search_by_word(word, instance):
