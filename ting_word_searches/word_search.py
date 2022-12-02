aaaa3e2b9d41a673988c1f8f8141ffea7ad4c56a
diegocamomila <diego.camomila@gmail.com>
2022-12-02 05:03:03 -0300

    FEAT: Implementará a funçao search_by_word em ting_word_searches/word_search.py

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -20,3 +20,20 @@ def exists_word(word, instance):
 
 def search_by_word(word, instance):
     """Aqui irá sua implementação"""
+    list_text = []
+    occurrences = []
+
+    for index, line in enumerate(instance._data[0]["linhas_do_arquivo"]):
+        if word.lower() in line.lower():
+            occurrences.append({"linha": index + 1, "conteudo": line})
+
+    if 0 < len(occurrences):
+        list_text.append(
+            {
+                "palavra": word,
+                "arquivo": instance._data[0]["nome_do_arquivo"],
+                "ocorrencias": occurrences,
+            }
+        )
+
+    return list_text
